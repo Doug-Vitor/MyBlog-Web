0cc88e398d599219ca391a43f5d5d93bda6f89ed
Douglas Vitor <douglasvitor.dev@gmail.com>
2021-11-30 11:28:35 -0300

    Initial commit

---
+++ src/BetterNews.Web/Program.cs
@@ -0,0 +1,24 @@
+var applicationBuilder = WebApplication.CreateBuilder(args);
+
+applicationBuilder.Services.AddControllersWithViews();
+
+var application = applicationBuilder.Build();
+
+if (!application.Environment.IsDevelopment())
+{
+    application.UseExceptionHandler("/Home/Error");
+    application.UseHsts();
+}
+
+application.UseHttpsRedirection();
+application.UseStaticFiles();
+
+application.UseRouting();
+
+application.UseAuthorization();
+
+application.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+application.Run();
